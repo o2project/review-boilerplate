a742874371920920ebb4b2f623bb4488b8d143af
kubosho <ta2@o2p.jp>
2016-06-28 19:15:59 +0900

    build(npm): ブラウザ上でプレビューしたりPDF出力をおこなえるようにした

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
-const exec = require('child_process').exec;
+const exec = require('child_process').exec
+const gulp = require('gulp')
+const del = require('del')
 
-const reviewPrefix = 'bundle exec '
-const reviewCompile = `${reviewPrefix}review-compile`
-const reviewWebMaker = `${reviewPrefix}review-webmaker`
-const reviewPdfMaker = `${reviewPrefix}review-pdfmaker`
-const reviewEpubMaker = `${reviewPrefix}review-epubmaker`
+const targetDir = 'articles'
+const webrootDir = 'webroot'
+
+const reviewConfig = 'config.yml'
+const reviewPrefix = 'bundle exec'
+const reviewCompile = `${reviewPrefix} review-compile`
+const reviewWebMaker = `${reviewPrefix} review-webmaker`
+const reviewPdfMaker = `${reviewPrefix} review-pdfmaker`
+const reviewEpubMaker = `${reviewPrefix} review-epubmaker`
+
+gulp.task('web', () => {
+  process.chdir(targetDir)
+  exec(`${reviewWebMaker} ${reviewConfig}`)
+})
+
+gulp.task('pdf', () => {
+  process.chdir(targetDir)
+  exec(`${reviewPdfMaker} ${reviewConfig}`)
+})
+
+gulp.task('clean', done => {
+  del([`${targetDir}/${webrootDir}`]).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'))
+    done()
+  })
+})
